d3e7e9b3831cf2f1a89242018c0ded7a99dad2cc
Dave Connors <dave.connors@fishtownanalytics.com>
2023-09-07 10:07:31 -0500

    add semantic model to cli param for resource type

--- core/dbt/cli/params.py
+++ core/dbt/cli/params.py
@@ -360,6 +360,7 @@ resource_type = click.option(
     type=ChoiceTuple(
         [
             "metric",
+            "semantic_model",
             "source",
             "analysis",
             "model",
