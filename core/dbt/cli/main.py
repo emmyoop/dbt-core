fb305a146b1601ede8e1fd46c87b5c8d64db98c2
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2023-09-19 14:05:21 +0100

    Fix #6497: Support global flags passed in after subcommands

--- core/dbt/cli/main.py
+++ core/dbt/cli/main.py
@@ -1,3 +1,4 @@
+import functools
 from copy import copy
 from dataclasses import dataclass
 from typing import Callable, List, Optional, Union
@@ -118,6 +119,48 @@ class dbtRunner:
             )
 
 
+# approach from https://github.com/pallets/click/issues/108#issuecomment-280489786
+def global_flags(func):
+    @click.pass_context
+    @p.cache_selected_only
+    @p.debug
+    @p.deprecated_print
+    @p.enable_legacy_logger
+    @p.fail_fast
+    @p.log_cache_events
+    @p.log_file_max_bytes
+    @p.log_format
+    @p.log_format_file
+    @p.log_level
+    @p.log_level_file
+    @p.log_path
+    @p.macro_debugging
+    @p.partial_parse
+    @p.partial_parse_file_path
+    @p.partial_parse_file_diff
+    @p.populate_cache
+    @p.print
+    @p.printer_width
+    @p.quiet
+    @p.record_timing_info
+    @p.send_anonymous_usage_stats
+    @p.single_threaded
+    @p.static_parser
+    @p.use_colors
+    @p.use_colors_file
+    @p.use_experimental_parser
+    @p.version
+    @p.version_check
+    @p.warn_error
+    @p.warn_error_options
+    @p.write_json
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 # dbt
 @click.group(
     context_settings={"help_option_names": ["-h", "--help"]},
@@ -125,39 +168,7 @@ class dbtRunner:
     no_args_is_help=True,
     epilog="Specify one of these sub-commands and you can find more help from there.",
 )
-@click.pass_context
-@p.cache_selected_only
-@p.debug
-@p.deprecated_print
-@p.enable_legacy_logger
-@p.fail_fast
-@p.log_cache_events
-@p.log_file_max_bytes
-@p.log_format
-@p.log_format_file
-@p.log_level
-@p.log_level_file
-@p.log_path
-@p.macro_debugging
-@p.partial_parse
-@p.partial_parse_file_path
-@p.partial_parse_file_diff
-@p.populate_cache
-@p.print
-@p.printer_width
-@p.quiet
-@p.record_timing_info
-@p.send_anonymous_usage_stats
-@p.single_threaded
-@p.static_parser
-@p.use_colors
-@p.use_colors_file
-@p.use_experimental_parser
-@p.version
-@p.version_check
-@p.warn_error
-@p.warn_error_options
-@p.write_json
+@global_flags
 def cli(ctx, **kwargs):
     """An ELT tool for managing your SQL transformations and data models.
     For more documentation on these commands, visit: docs.getdbt.com
@@ -166,11 +177,9 @@ def cli(ctx, **kwargs):
 
 # dbt build
 @cli.command("build")
-@click.pass_context
 @p.defer
 @p.deprecated_defer
 @p.exclude
-@p.fail_fast
 @p.favor_state
 @p.deprecated_favor_state
 @p.full_refresh
@@ -190,7 +199,6 @@ def cli(ctx, **kwargs):
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -212,7 +220,7 @@ def build(ctx, **kwargs):
 
 # dbt clean
 @cli.command("clean")
-@click.pass_context
+@global_flags
 @p.profile
 @p.profiles_dir
 @p.project_dir
@@ -234,14 +242,14 @@ def clean(ctx, **kwargs):
 
 # dbt docs
 @cli.group()
-@click.pass_context
+@global_flags
 def docs(ctx, **kwargs):
     """Generate or serve the documentation website for your project"""
 
 
 # dbt docs generate
 @docs.command("generate")
-@click.pass_context
+@global_flags
 @p.compile_docs
 @p.defer
 @p.deprecated_defer
@@ -261,7 +269,6 @@ def docs(ctx, **kwargs):
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -283,7 +290,7 @@ def docs_generate(ctx, **kwargs):
 
 # dbt docs serve
 @docs.command("serve")
-@click.pass_context
+@global_flags
 @p.browser
 @p.port
 @p.profile
@@ -311,7 +318,7 @@ def docs_serve(ctx, **kwargs):
 
 # dbt compile
 @cli.command("compile")
-@click.pass_context
+@global_flags
 @p.defer
 @p.deprecated_defer
 @p.exclude
@@ -335,7 +342,6 @@ def docs_serve(ctx, **kwargs):
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -358,7 +364,7 @@ def compile(ctx, **kwargs):
 
 # dbt show
 @cli.command("show")
-@click.pass_context
+@global_flags
 @p.defer
 @p.deprecated_defer
 @p.exclude
@@ -382,7 +388,6 @@ def compile(ctx, **kwargs):
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -405,7 +410,7 @@ def show(ctx, **kwargs):
 
 # dbt debug
 @cli.command("debug")
-@click.pass_context
+@global_flags
 @p.debug_connection
 @p.config_dir
 @p.profile
@@ -413,7 +418,6 @@ def show(ctx, **kwargs):
 @p.project_dir
 @p.target
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 def debug(ctx, **kwargs):
@@ -431,7 +435,7 @@ def debug(ctx, **kwargs):
 
 # dbt deps
 @cli.command("deps")
-@click.pass_context
+@global_flags
 @p.profile
 @p.profiles_dir_exists_false
 @p.project_dir
@@ -451,7 +455,7 @@ def deps(ctx, **kwargs):
 
 # dbt init
 @cli.command("init")
-@click.pass_context
+@global_flags
 # for backwards compatibility, accept 'project_name' as an optional positional argument
 @click.argument("project_name", required=False)
 @p.profile
@@ -473,7 +477,7 @@ def init(ctx, **kwargs):
 
 # dbt list
 @cli.command("list")
-@click.pass_context
+@global_flags
 @p.exclude
 @p.indirect_selection
 @p.models
@@ -518,7 +522,7 @@ cli.add_command(ls, "ls")
 
 # dbt parse
 @cli.command("parse")
-@click.pass_context
+@global_flags
 @p.profile
 @p.profiles_dir
 @p.project_dir
@@ -526,7 +530,6 @@ cli.add_command(ls, "ls")
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -542,13 +545,12 @@ def parse(ctx, **kwargs):
 
 # dbt run
 @cli.command("run")
-@click.pass_context
+@global_flags
 @p.defer
 @p.deprecated_defer
 @p.favor_state
 @p.deprecated_favor_state
 @p.exclude
-@p.fail_fast
 @p.full_refresh
 @p.profile
 @p.profiles_dir
@@ -562,7 +564,6 @@ def parse(ctx, **kwargs):
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -584,7 +585,7 @@ def run(ctx, **kwargs):
 
 # dbt retry
 @cli.command("retry")
-@click.pass_context
+@global_flags
 @p.project_dir
 @p.profiles_dir
 @p.vars
@@ -592,7 +593,6 @@ def run(ctx, **kwargs):
 @p.target
 @p.state
 @p.threads
-@p.fail_fast
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -614,7 +614,7 @@ def retry(ctx, **kwargs):
 
 # dbt clone
 @cli.command("clone")
-@click.pass_context
+@global_flags
 @p.defer_state
 @p.exclude
 @p.full_refresh
@@ -629,7 +629,6 @@ def retry(ctx, **kwargs):
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.preflight
 @requires.profile
 @requires.project
@@ -651,7 +650,7 @@ def clone(ctx, **kwargs):
 
 # dbt run operation
 @cli.command("run-operation")
-@click.pass_context
+@global_flags
 @click.argument("macro")
 @p.args
 @p.profile
@@ -682,7 +681,7 @@ def run_operation(ctx, **kwargs):
 
 # dbt seed
 @cli.command("seed")
-@click.pass_context
+@global_flags
 @p.exclude
 @p.full_refresh
 @p.profile
@@ -698,7 +697,6 @@ def run_operation(ctx, **kwargs):
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
@@ -719,7 +717,7 @@ def seed(ctx, **kwargs):
 
 # dbt snapshot
 @cli.command("snapshot")
-@click.pass_context
+@global_flags
 @p.defer
 @p.deprecated_defer
 @p.exclude
@@ -758,14 +756,14 @@ def snapshot(ctx, **kwargs):
 
 # dbt source
 @cli.group()
-@click.pass_context
+@global_flags
 def source(ctx, **kwargs):
     """Manage your project's sources"""
 
 
 # dbt source freshness
 @source.command("freshness")
-@click.pass_context
+@global_flags
 @p.exclude
 @p.output_path  # TODO: Is this ok to re-use?  We have three different output params, how much can we consolidate?
 @p.profile
@@ -807,11 +805,10 @@ cli.commands["source"].add_command(snapshot_freshness, "snapshot-freshness")  # 
 
 # dbt test
 @cli.command("test")
-@click.pass_context
+@global_flags
 @p.defer
 @p.deprecated_defer
 @p.exclude
-@p.fail_fast
 @p.favor_state
 @p.deprecated_favor_state
 @p.indirect_selection
@@ -828,7 +825,6 @@ cli.commands["source"].add_command(snapshot_freshness, "snapshot-freshness")  # 
 @p.target_path
 @p.threads
 @p.vars
-@p.version_check
 @requires.postflight
 @requires.preflight
 @requires.profile
